3ea5fa7d34fd965e2620d6309ebf486d4fddebfa
huangyuanhui <2278181701@qq.com>
2022-07-11 22:58:05 +0800

    Nacos配置管理： 微服务启动时会从Nacos读取多个配置文件： 1：[spring.application.name]-[spring.profiles.active].yaml，例如：user-service-dev.yaml，即环境配置 2：[spring.application.name].yaml，例如：user-service.yaml，即默认配置，多环境共享 无论profile如何变化，[spring.application.name].yaml这个文件是一定会加载的，因此多环境共享配置可以写入这个文件！
    多种配置的优先级：
    （nacos中当前环境配置）服务名-profile.yaml  > （nacos中共享配置）服务名.yaml > 本地配置

--- user-service/src/main/java/com/cloud/user/config/PatternProperties.java
+++ user-service/src/main/java/com/cloud/user/config/PatternProperties.java
@@ -2,12 +2,26 @@ package com.cloud.user.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-@Configuration
+/*
+Nacos配置管理：
+微服务启动时会从Nacos读取多个配置文件：
+1：[spring.application.name]-[spring.profiles.active].yaml，例如：user-service-dev.yaml
+2：[spring.application.name].yaml，例如：user-service.yaml
+无论profile如何变化，[spring.application.name].yaml这个文件是一定会加载的，因此多环境共享配置可以写入这个文件！
+
+多种配置的优先级：
+（nacos中当前环境配置）服务名-profile.yaml  > （nacos中共享配置）服务名.yaml > 本地配置
+ */
+@Component
 @ConfigurationProperties(prefix = "pattern")
 @Data
 public class PatternProperties {
 
     private String dateformat;
+
+    private String envShareValue;
+
+    private String name;
 }
--- user-service/src/main/java/com/cloud/user/controller/UserController.java
+++ user-service/src/main/java/com/cloud/user/controller/UserController.java
@@ -4,6 +4,7 @@ import com.cloud.user.config.PatternProperties;
 import com.cloud.user.model.User;
 import com.cloud.user.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 
 @RequestMapping("/users")
 @RestController
@@ -27,9 +30,32 @@ public class UserController {
     @Autowired
     private PatternProperties properties;
 
+    /**
+     * 统一配置管理
+     *
+     * @return
+     */
     @GetMapping("/now")
     public String now() {
         return LocalDateTime.now().format(DateTimeFormatter.ofPattern(properties.getDateformat()));
+    }
+
+    /**
+     * 多环境共享配置
+     *
+     * @return
+     */
+    @GetMapping("/props")
+    public Map<String, String> props() {
+        Map<String, String> propsMap = new HashMap<>();
+        if (StringUtils.hasText(properties.getDateformat())) {
+            propsMap.put("dateformat", LocalDateTime.now().format(DateTimeFormatter.ofPattern(properties.getDateformat())));
+        } else {
+            propsMap.put("deteformat", null);
+        }
+        propsMap.put("envShareValue", properties.getEnvShareValue());
+        propsMap.put("name", properties.getName());
+        return propsMap;
     }
 
 }
