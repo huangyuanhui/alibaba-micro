2909075c1893e90756cddd5bf7aa78515e7a7d3a
huangyuanhui <2278181701@qq.com>
2022-07-11 20:54:12 +0800

    Nacos注册中心原理 ：      服务提供者再启动的时候会把自己的信息注册到注册中心，服务消费者会定时向注册中心拉取服务列表，拉取到服务列表之后发起远程调用。      Nacos对比Eureka的优点是：当服务列表发生变更时，Nacos会主动推送服务列表，即PULL与PUSH结合，服务列表更新及时、效率更高。
         nacos中，服务分为临时实例 与 非临时实例，默认情况下，实例是非临时实例
         1：临时实例的健康检测是：临时实例会主动定期向注册中心发送心跳，实现服务的健康检测，当一段时间临时实例没有心跳上来，Nacos会把临时实例从服务列表中剔除：舔狗！
         2：非临时实例的健康检测则是：由Nacos主动去发请求询问非临时实例的健康状态，即使非临时实例挂掉了，Nacos也不会把非临时实例在服务列表中剔除：非舔狗！
    
     Nacos与Eureka的共同点：
     都支持服务注册和服务拉取；
     都支持服务提供者心跳方式做健康检测。
    
     Nacos与Eureka的区别：
     Nacos支持服务端主动检测提供者状态：临时实例采用心跳模式，非临时实例采用主动检测模式;
     临时实例心跳不正常会被剔除，非临时实例则不会被剔除;
     Nacos支持服务列表变更的消息推送模式，服务列表更新更及时；
     Nacos集群默认采用AP方式，当集群存在非临时实例时，采用CP模式；Eureka采用AP方式。

--- order-service/src/main/java/com/cloud/order/OrderServiceApplication.java
+++ order-service/src/main/java/com/cloud/order/OrderServiceApplication.java
@@ -14,6 +14,20 @@ public class OrderServiceApplication {
         SpringApplication.run(OrderServiceApplication.class, args);
     }
 
+    /*
+     Nacos注册中心原理 ：
+     服务提供者再启动的时候会把自己的信息注册到注册中心，服务消费者会定时向注册中心拉取服务列表，拉取到服务
+     列表之后发起远程调用。
+     Nacos对比Eureka的优点是：当服务列表发生变更时，Nacos会主动推送服务列表，即PULL与PUSH结合，
+     服务列表更新及时、效率更高。
+
+     nacos中，服务分为临时实例 与 非临时实例，默认情况下，实例是非临时实例
+     1：临时实例的健康检测是：临时实例会主动定期向注册中心发送心跳，实现服务的健康检测，当一段时间临时实例没有心跳上来，Nacos
+     会把临时实例从服务列表中剔除：舔狗
+     2：非临时实例的健康检测则是：由Nacos主动去发请求询问非临时实例的健康状态，即使非临时实例挂掉了，
+     Nacos也不会把非临时实例在服务列表中剔除：非舔狗
+
+     */
     @Bean
     @LoadBalanced
     public RestTemplate restTemplate() {
